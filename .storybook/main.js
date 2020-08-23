109282e79c9edba8685dd4869d3a3ce39b939589
mulder21c <mulder21c@gmail.com>
2020-08-23 13:26:31 +0900

    Storybook Ininital Example
    - Button Component As Example
    - Use *.MDX
    - Use Essentails instead of knobs
    - Add addon-a11y

---
+++ .storybook/main.js
@@ -0,0 +1,38 @@
+const path = require(`path`);
+const rootPath = path.resolve(__dirname, `../`);
+
+module.exports = {
+  stories: [
+    `../components/**/*.stories.mdx`,
+    `../components/**/*.stories.@(js|jsx|ts|tsx)`
+  ],
+  addons: [
+    `@storybook/addon-links`,
+    `@storybook/addon-essentials`,
+    `@storybook/addon-a11y`,
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias['@'] = rootPath;
+    config.resolve.alias['~'] = rootPath;
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            additionalData: `
+              @import "@/assets/scss/helpers/_functions.scss";
+              @import "@/assets/scss/modules/_variables.scss";
+            `,
+          },
+        }
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    return config;
+  },
+};
---
+++ components/atoms/Button/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <button
+    :type="type"
+    :class="[
+      `btn`,
+      `btn--${context}`,
+      {'btn--round-edge': isRoundEdge},
+      {'btn--block': isBlock}
+    ]"
+    :disabled="isDisabled"
+  >
+    <!-- @slot Button Content -->
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+      default: 'button',
+      validator: (val) => {
+        return ['button', 'submit', 'reset'].includes(val)
+      }
+    },
+    context: {
+      type: String,
+      required: false,
+      default: 'default'
+    },
+    isDisabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    isRoundEdge: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    isBlock: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped src="./index.scss"></style>
