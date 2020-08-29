44015e48ad32f87a9508779eccb448123a2256d7
mulder21c <mulder21c@gmail.com>
2020-08-29 18:00:20 +0900

    nuxt-link 예제 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,15 @@
+import Vue from 'vue';
+import { action } from '@storybook/addon-actions';
+
+Vue.component('nuxt-link', {
+  props: [`to`],
+  methods: {
+    log() {
+      action(`link target`)(this.to);
+    },
+  },
+  template: '<a href="#" @click.prevent="log()"><slot /></a>',
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
---
+++ components/molecules/LinksGroup/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nuxt-link v-for="(nav, index) in navList" :key="index" :to="nav.path" class="link">
+      {{ nav.label }}
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    navList: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped src="./index.scss"></style>
