bcd9ac1cb7b4f8eee28c07ced57c99675b6c8212
mulder21c <mulder21c@gmail.com>
2020-10-03 16:12:22 +0900

    Stories 수정 및 utils stories 추가
    - Utils Stories (colors, texts) 추가 (json-to-sass 관련 예제)
    - Story title 잘못 된 것들 수정
    - storybook 관련 코멘트 추가
    - 오기 수정 ('isHeadingVisuallyHidden' -> 'isVisuallyHiddenHeading')

---
+++ components/utils/Colors/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="['palette', color ? `palette${color}` : '']">
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    </p>
+    <div class="rect-angles">
+      <div class="rect-angle rect-angle--small" />
+      <div class="rect-angle rect-angle--medium" />
+      <div class="rect-angle rect-angle--large" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: '',
+      required: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped src="./index.scss"></style>
---
+++ components/utils/Texts/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p
+      :class="[
+        'text-box',
+        fontSize ? `text-box--${fontSize}` : '',
+        fontWeight ? `text-box--${fontWeight}` : ''
+      ]"
+    >
+      {{ content }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    fontSize: {
+      type: String,
+      required: false,
+      default: 'base'
+    },
+    fontWeight: {
+      type: String,
+      required: false,
+      default: 'regular'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped src="./index.scss"></style>
